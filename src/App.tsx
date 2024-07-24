2fefd2c7d24f236e91893fc7d7d0cb3d13d0147a
Bryan Ross <git@rossipedia.com>
2024-07-23 19:15:37 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import Container from '@mui/material-pigment-css/Container';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h2">Hello</Typography>
+    </Container>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { pigment } from '@pigment-css/vite-plugin';
+import { extendTheme } from '@mui/material';
+
+const pigmentConfig = {
+  theme: extendTheme(),
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), pigment(pigmentConfig)],
+});
